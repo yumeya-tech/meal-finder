092b439e0c976c09b9ce1ac0b10425adf92cf994
yumeya.tech <qq5y5bsd@gmail.com>
2025-01-19 10:39:05 +0900

    スタイルとコンポーネント構成のリファクタリング
    グローバルCSSを整理し、スタイルを`styles`ディレクトリに移動。`Header`および`Form`コンポーネントを追加し、`App.tsx`を簡潔化して構造を改善。

---
+++ src/components/Form.tsx
@@ -0,0 +1,9 @@
+const Form = () => {
+  return (
+    <form>
+      <input type="text" name="mealName" placeholder="料理名を英語で入力" />
+      <button>検索</button>
+    </form>
+  );
+};
+export default Form;
